a4fd4c840a94952505b6a8c96a38550c19bde357
Pieter Iepsma <pieter.iepsma@gmail.com>
2025-05-08 14:08:34 +0200

    feat: Initial setup

---
+++ schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { devExperience } from "./devExperience";
+import { projects } from "./project";
+import { skills } from "./skill";
+
+export const schemaTypes = [devExperience, projects, skills]
